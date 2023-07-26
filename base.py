1bd379f04932763594cef7dcc95e12450a888588
KienHuynh <hckien@hcmut.edu.vn>
2023-07-26 18:13:37 -0400

    Move most common functions from two_sweeping_approx to base

--- base.py
+++ base.py
@@ -3,6 +3,14 @@ This file provides basic functions to check for line segment intersection
 and for checking the validity of a diagonal in a simple polygon
 """
 
+from typing import List
+from config import *
+import sys
+import os
+import subprocess
+import numpy as np
+
+
 def area2(a, b, c):
     return (b[0] - a[0]) * (c[1] - a[1]) - (c[0] - a[0]) * (b[1] - a[1])
 
@@ -94,3 +102,105 @@ def diagonal(a, b, verts):
     return in_cone(a, b, verts) and in_cone(b, a, verts) and diagonalie(a, b, verts)
 
 
+def poly_vis_cgal_interior(verts: List[List[float]], query: [List[float]]) -> List[List[List[float]]]:
+    """
+    Computing visibility polygon of a query point (interior)
+    :param verts: Vertices of the polygon [[x1, y1], [x2, y2], ...], counter-clockwise.
+    :param query: Query point [x, y]
+    :return: vertices of the visibility polygon
+    """
+    arg = poly_vis_path + ' '
+    arg += '--verts='
+    for v in verts:
+        arg += str(v[0]) + ',' + str(v[1]) + ','
+    arg = arg[:-1] + ' '
+    arg += '--query=' + str(query[0]) + ',' + str(query[1]) + ' '
+    arg += '--boundary=false'
+
+    print('Running ' + arg)
+
+    if os.name == 'posix':
+        popen = subprocess.Popen(arg, stdout=subprocess.PIPE, shell=True)
+    if os.name == 'nt':
+        popen = subprocess.Popen(arg, stdout=subprocess.PIPE)
+
+    popen.wait()
+    output = popen.stdout.read().decode("utf-8")
+    output = output.split()
+    output = [float(o) for o in output]
+
+    vis_poly = []
+    for i in range(0, len(output), 2):
+        vis_poly.append([output[i], output[i+1]])
+
+    return vis_poly
+
+
+def poly_vis_cgal_boundary(verts: List[List[float]], query: [List[float]], pre_query: [List[float]]) -> List[List[List[float]]]:
+    """
+    Computing visibility polygon of a query point in a polygon with point on the boundary
+    :param verts: Vertices of the polygon [[x1, y1], [x2, y2], ...], counter-clockwise.
+    :param query: Query point [x, y]
+    :param pre_query: Point on the boundary of the polygon preceding the query point
+    :return: vertices of the visibility polygon
+    """
+    arg = poly_vis_path + ' '
+    arg += '--verts='
+    for v in verts:
+        arg += str(v[0]) + ',' + str(v[1]) + ','
+    arg = arg[:-1] + ' '
+    arg += '--query=' + str(query[0]) + ',' + str(query[1]) + ' '
+    arg += '--pre_query=' + str(pre_query[0]) + ',' + str(pre_query[1]) + ' '
+    arg += '--boundary=true'
+
+    if os.name == 'posix':
+        popen = subprocess.Popen(arg, stdout=subprocess.PIPE, shell=True)
+    if os.name == 'nt':
+        popen = subprocess.Popen(arg, stdout=subprocess.PIPE)
+
+    popen.wait()
+    output = popen.stdout.read().decode("utf-8")
+    output = output.split()
+    output = [float(o) for o in output]
+
+    vis_poly = []
+    for i in range(0, len(output), 2):
+        vis_poly.append([output[i], output[i+1]])
+
+    return vis_poly
+
+
+def above_below(p, n, a, use_normal=True):
+    """
+    Return a positive value if p is above the line xn - an = 0, negative otherwise
+    If n is not a normal vector, it must be the tangent vector of the line
+    :param p: numpy 2D vector, point for testing
+    :param n: numpy 2D vector, normal vector
+    :param a: numpy 2D vector
+    :return:
+    """
+    if use_normal:
+        return (p - a)@n #p@n - a@n
+    else:
+        n_ = [n[1], 0-n[0]]
+        return p@n_ - a@n_
+
+
+def get_norm(v):
+    """
+    Calcualte the norm (i.e. length) of a vector, l2
+    :param v: [x, y]
+    :return: float value
+    """
+    return np.sqrt(v[0]**2 + v[1]**2)
+
+
+def get_cos(u, v):
+    """
+    Get cosine distance between two vectors (in rad)
+    :param u: [x, y]
+    :param v: [x, y]
+    :return:
+    """
+    product = u[0]*v[0] + u[1]*v[1]
+    return product / (get_norm(u) * get_norm(v))
--- two_sweeping_approx.py
+++ two_sweeping_approx.py
@@ -12,246 +12,14 @@ import time
 
 from typing import List
 from base import *
-
-poly_vis_path = './cgal_util/poly_vis_cgal/poly_vis_cgal'
+from pslg import *
+from config import *
 
 
 def draw_polygon(polygon):
     n = len(polygon)
     plt.plot(polygon[:, 0], polygon[:, 1], 'k')
     plt.plot(polygon[[n-1, 0], 0], polygon[[n-1, 0], 1], 'k')
-
-
-class Node:
-    def __init__(self, _id, _pos):
-        """
-        2D position of the node
-        :param _id:
-        :param _pos:
-        """
-        self.id = _id
-        self.pos = _pos
-        self.edges = []
-        self.visited = False
-
-
-class Edge:
-    def __init__(self, _from: Node, _to: Node, _type):
-        """
-        :param _from: Node object
-        :param _to: Node onject
-        :param _type: "poly" or "vis"
-        """
-        self.src = _from
-        self.to = _to
-        self.type = _type
-
-
-class PSLG:
-    def __init__(self, polygon):
-        """
-
-        :param polygon: Vertices of the polygon [[x1, y1], [x2, y2], ...], counter-clockwise
-        """
-        self.nodes = {}
-        self.edges = {}
-        self.edge_set = []
-        n = len(polygon)
-        for i in range(n):
-            self.nodes[i] = Node(i, polygon[i])
-        for i in range(n):
-            edge = Edge(self.nodes[i], self.nodes[(i + 1) % n], "poly")
-            self.edges[i] = [edge]
-            self.edge_set.append(edge)
-
-    def add_node(self, Node):
-        num_node = len(self.nodes)
-        self.nodes[num_node] = Node
-        self.edges[num_node] = []
-
-
-    def add_edge(self, _e):
-        self.edge_set.append(_e)
-        self.edges[_e.src.id].append(_e)
-
-
-    def remove_edge(self, _e: Edge):
-        """
-        Remove edge _e from the edge set and the adjacency list
-        :param _e:
-        :return:
-        """
-        # Remove edge _e from the edge superset
-        self.edge_set.remove(_e)
-
-        # Remove edge _e from the adjacency list
-        self.edges[_e.src.id].remove(_e)
-
-
-    def draw_edge_set(self):
-        for e in self.edge_set:
-            c = 'k'
-            if e.type == 'vis':
-                c = 'b'
-
-            src = e.src.pos
-            to = e.to.pos
-            plt.plot([src[0], to[0]], [src[1], to[1]], color=c)
-
-
-    def draw_adj_list(self):
-        for k, es in self.edges.items():
-            for e in es:
-                c = 'k'
-                if e.type == 'vis':
-                    c = 'b'
-
-                src = e.src.pos
-                to = e.to.pos
-                plt.plot([src[0], to[0]], [src[1], to[1]], color=c)
-
-
-    def node_exist(self, pos):
-        for k, v in self.nodes:
-            if v[0] == pos[0] and v[1] == pos[1]:
-                return True
-
-        return False
-
-
-    def get_node(self, pos):
-        for k, v in self.nodes.items():
-            if v.pos[0] == pos[0] and v.pos[1] == pos[1]:
-                return v
-
-        return None
-
-
-    def add_intersection(self, pos, edge, ray_start):
-        # If this is an actual new node
-        old_node = self.get_node(pos)
-        if (old_node == None):
-            node = Node(len(self.nodes), pos)
-
-            # Remove previous edge and add 4 new edges
-            self.add_node(node)
-            self.remove_edge(edge)
-            self.add_edge(Edge(edge.src, node, 'poly'))
-            self.add_edge(Edge(node, edge.to, 'poly'))
-            self.add_edge(Edge(node, self.nodes[ray_start], 'vis'))
-            self.add_edge(Edge(self.nodes[ray_start], node, 'vis'))
-            return node
-
-        else:
-            self.add_edge(Edge(old_node, self.nodes[ray_start], 'vis'))
-            self.add_edge(Edge(self.nodes[ray_start], old_node, 'vis'))
-            return old_node
-
-
-
-
-def poly_vis_cgal_interior(verts: List[List[float]], query: [List[float]]) -> List[List[List[float]]]:
-    """
-    Computing visibility polygon of a query point (interior)
-    :param verts: Vertices of the polygon [[x1, y1], [x2, y2], ...], counter-clockwise.
-    :param query: Query point [x, y]
-    :return: vertices of the visibility polygon
-    """
-    arg = poly_vis_path + ' '
-    arg += '--verts='
-    for v in verts:
-        arg += str(v[0]) + ',' + str(v[1]) + ','
-    arg = arg[:-1] + ' '
-    arg += '--query=' + str(query[0]) + ',' + str(query[1]) + ' '
-    arg += '--boundary=false'
-
-    print('Running ' + arg)
-
-    if os.name == 'posix':
-        popen = subprocess.Popen(arg, stdout=subprocess.PIPE, shell=True)
-    if os.name == 'nt':
-        popen = subprocess.Popen(arg, stdout=subprocess.PIPE)
-
-    popen.wait()
-    output = popen.stdout.read().decode("utf-8")
-    output = output.split()
-    output = [float(o) for o in output]
-
-    vis_poly = []
-    for i in range(0, len(output), 2):
-        vis_poly.append([output[i], output[i+1]])
-
-    return vis_poly
-
-
-def poly_vis_cgal_boundary(verts: List[List[float]], query: [List[float]], pre_query: [List[float]]) -> List[List[List[float]]]:
-    """
-    Computing visibility polygon of a query point in a polygon with point on the boundary
-    :param verts: Vertices of the polygon [[x1, y1], [x2, y2], ...], counter-clockwise.
-    :param query: Query point [x, y]
-    :param pre_query: Point on the boundary of the polygon preceding the query point
-    :return: vertices of the visibility polygon
-    """
-    arg = poly_vis_path + ' '
-    arg += '--verts='
-    for v in verts:
-        arg += str(v[0]) + ',' + str(v[1]) + ','
-    arg = arg[:-1] + ' '
-    arg += '--query=' + str(query[0]) + ',' + str(query[1]) + ' '
-    arg += '--pre_query=' + str(pre_query[0]) + ',' + str(pre_query[1]) + ' '
-    arg += '--boundary=true'
-
-    if os.name == 'posix':
-        popen = subprocess.Popen(arg, stdout=subprocess.PIPE, shell=True)
-    if os.name == 'nt':
-        popen = subprocess.Popen(arg, stdout=subprocess.PIPE)
-
-    popen.wait()
-    output = popen.stdout.read().decode("utf-8")
-    output = output.split()
-    output = [float(o) for o in output]
-
-    vis_poly = []
-    for i in range(0, len(output), 2):
-        vis_poly.append([output[i], output[i+1]])
-
-    return vis_poly
-
-
-def above_below(p, n, a, use_normal=True):
-    """
-    Return a positive value if p is above the line xn - an = 0, negative otherwise
-    If n is not a normal vector, it must be the tangent vector of the line
-    :param p: numpy 2D vector, point for testing
-    :param n: numpy 2D vector, normal vector
-    :param a: numpy 2D vector
-    :return:
-    """
-    if use_normal:
-        return (p - a)@n #p@n - a@n
-    else:
-        n_ = [n[1], 0-n[0]]
-        return p@n_ - a@n_
-
-
-def get_norm(v):
-    """
-    Calcualte the norm (i.e. length) of a vector, l2
-    :param v: [x, y]
-    :return: float value
-    """
-    return np.sqrt(v[0]**2 + v[1]**2)
-
-
-def get_cos(u, v):
-    """
-    Get cosine distance between two vectors (in rad)
-    :param u: [x, y]
-    :param v: [x, y]
-    :return:
-    """
-    product = u[0]*v[0] + u[1]*v[1]
-    return product / (get_norm(u) * get_norm(v))
 
 
 def find_lowest_vert_intersection(p, d, pslg: PSLG):
@@ -395,6 +163,7 @@ def compute_histogram(polygon, e):
         ai = node.id
         bi = edge.to.id
 
+
     # Start going around P, compute the projected image of each reflex vertex onto the boundary of P with direction "up"
     while bi != a0:
         bj = (bi + 1) % n
@@ -415,8 +184,6 @@ def compute_histogram(polygon, e):
         ai = bi
         bi = bj
 
-
-
     # Take special care of the last edge
     bj = b0
     # Check if ai is below or above a0b0
@@ -435,10 +202,8 @@ def compute_histogram(polygon, e):
     plt.close()
 
     # Retrieve the parent histogram polygon, as well as the children subpolygons
-
-
-
     #return main_hist
+
 
 def test_vis():
     #polygon = [[0, 0], [1, 1], [2, 0], [2, 4], [1, 2], [0, 4]]
